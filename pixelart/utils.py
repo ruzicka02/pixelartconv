7b691310ea145d334ba41f8138f6111ea7d2bef8
Simon Ruzicka <ruzicsi1@fit.cvut.cz>
2023-01-25 17:27:22 +0100

    IO functions

--- pixelart/utils.py
+++ pixelart/utils.py
@@ -1,5 +1,5 @@
 """
-Utility functions for the script.
+IO functions for the script.
 """
 
 from pathlib import Path
@@ -20,13 +20,17 @@ def image_prepare(path: Path, dims: tuple) -> np.ndarray:
     image = ImageOps.fit(image, dims)
     image = np.asarray(image)
 
+    assert image.shape == (*dims, 3)
+
     return image
 
 
 def load_image(name: str, dims: tuple = (10, 10), percent_filled: float = 0.7):
     """
-    Loads a PNG image from the `saves/images` directory, performs various operations
+    Loads a PNG image from the `img` directory, performs various operations
     and returns it as an array. In case something goes wrong, returns None.
+
+    :return: numpy.ndarray or None
     """
     name += ".png"
     path = Path(__file__).parent.parent
@@ -34,5 +38,38 @@ def load_image(name: str, dims: tuple = (10, 10), percent_filled: float = 0.7):
     try:
         image = image_prepare(path, dims)
         return image
-    except (FileNotFoundError, UnidentifiedImageError, ValueError, AssertionError):
+    except (FileNotFoundError, UnidentifiedImageError, ValueError, AssertionError) as err:
+        print(err)
         return None
+
+
+def save_image(data: np.ndarray, name: str = "export"):
+    name += ".png"
+    path = Path(__file__).parent.parent
+    path = (path / name).resolve()
+
+    image = Image.fromarray(np.uint8(data)).convert('RGB')
+    image.save(path)
+
+
+def load_colors(name: str):
+    """
+    Loads color data from a text file in the `img` directory.
+
+    :param str name: File name.
+    :return: List of tuples (color codes, length 3) or None
+    """
+
+    name += ".txt"
+    path = Path(__file__).parent.parent
+    path = (path / 'img' / name).resolve()
+
+    if not path.is_file():
+        print("File not found - aborting operation")
+        return None
+
+    with open(path, 'r') as file:
+        lines = [line.rstrip().lstrip('#') for line in file]
+
+    color_codes = [tuple(int(ln[i:i + 2], 16) for i in (0, 2, 4)) for ln in lines]
+    return color_codes
