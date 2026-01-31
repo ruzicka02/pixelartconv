953293a2caf9f85f10dbd7625363d0fa8b4980d7
Simon Ruzicka <simon.ruzicka@protonmail.com>
2026-01-31 20:19:25 +0100

    feat: Argparse

--- src/pixelartconv/__main__.py
+++ src/pixelartconv/__main__.py
@@ -1,38 +1,33 @@
 import sys
+import argparse
 from pathlib import Path
 
 import pixelartconv
 
-MORE_INFO = "For more info, type:\n    python -m pixelartconv --help"
-
 
 def main():
-    if len(sys.argv) not in [2, 3, 4]:
-        print("""Invalid syntax, please use one of the following:
-        python -m pixelartconv [file_name]
-        python -m pixelartconv [file_name] [width] [height]
-        """)
-        print(MORE_INFO)
-        sys.exit(1)
+    path = (Path(__file__).parent / "help.txt").resolve()
+    with open(path) as f:
+        help_text = f.read()
 
-    file_name = sys.argv[1]
-    height = int(sys.argv[2]) if len(sys.argv) > 2 else 64
-    width = int(sys.argv[3]) if len(sys.argv) > 3 else None  # else... will be calculated when image is loaded
-    dims = (width, height)
+    parser = argparse.ArgumentParser(
+        description="Convert images to pixel art with a limited color palette.",
+        epilog=help_text,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("file_name", type=str, help="Name of file(s) or their relative path.")
+    parser.add_argument("height", type=int, nargs="?", default=64, help="Target height of the resulting image.")
+    parser.add_argument("width", type=int, nargs="?", default=None, help="Target width of the resulting image.")
 
-    if "--help" in sys.argv or "-h" in sys.argv:
-        path = (Path(__file__).parent / "help.txt").resolve()
-        with open(path) as f:
-            print(f.read())
-        sys.exit(2)
+    # file_name = sys.argv[1]
+    # height = int(sys.argv[2]) if len(sys.argv) > 2 else 64
+    # width = int(sys.argv[3]) if len(sys.argv) > 3 else None  # else... will be calculated when image is loaded
 
-    if file_name[0] == "-":
-        print("Unknown command option.")
-        print(MORE_INFO)
-        sys.exit(1)
+    args = parser.parse_args()
+    dims = (args.width, args.height)
 
     try:
-        pixelartconv.convert(file_name, dims)
+        pixelartconv.convert(args.file_name, dims)
     except (ValueError, FileNotFoundError):  # correct exit code when issues encountered
         sys.exit(1)
 
